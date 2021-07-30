2729789de9c1a8900fb080dbf04e7492b3505e06
ZhangShuaifeng <71601420+ZSFsmile@users.noreply.github.com>
2021-07-30 10:35:35 +0800

    Update threadpool.c

--- threadpool.c
+++ threadpool.c
@@ -19,43 +19,47 @@
 } while(0)
 
 struct nTask{
-	void (*task_func)(void *arg);
-	void *user_data;
+	void (*task_func)(void *arg); //任务的功能
+	void *user_data;			 //任务相应的参数
 
 	struct nTask* prev;
 	struct nTask* next;
 };
 struct nWorker{
-	pthread_t threadid;
-	struct nManager *manager;
-	int terminate;
+	pthread_t threadid;        //线程id
+	struct nManager *manager;  //管理组件 方便从任务上直接操纵线程池
+	int terminate;			   //线程函数执行任务循环的退出的标志
 
-	struct nWorker* prev;
+	struct nWorker* prev;	
 	struct nWorker* next;
 };
 
 typedef struct nManager{
-	struct nTask* tasks;
-	struct nWorker* workers;
+	struct nTask* tasks;      //任务队列
+	struct nWorker* workers;  //执行队列
 
-	pthread_mutex_t mutex;
-	pthread_cond_t cond;
+	pthread_mutex_t mutex;		//互斥锁
+	pthread_cond_t cond;	 //条件变量
 }ThreadPool;
 
-// 回调函数callback != task
+// 回调函数callback != task 
+// 每一个任务的回调函数都相同
 static void *nThreadPoolCallback(void *arg) {
 	struct nWorker* worker = (struct nWorker*)arg;
 	while(1){
+		//在临界区上加锁
 		pthread_mutex_lock(&worker->manager->mutex);
 		//任务队列没有任务，就一直等待
 		while(worker->manager->tasks == NULL){
 			if(worker->terminate)break;
 			pthread_cond_wait(&worker->manager->cond,&worker->manager->mutex);
 		}
+		//退出的标志
 		if(worker->terminate){
 			pthread_mutex_unlock(&worker->manager->mutex);
 			break;
 		}
+		//移除链表的第一个结点
 		struct nTask *task = worker->manager->tasks;
 		LIST_REMOVE(task,worker->manager->tasks);
 		pthread_mutex_unlock(&worker->manager->mutex);
@@ -77,8 +81,9 @@ int nThreadPoolCreate(ThreadPool* pool,int numWorkers){
 
 	pthread_mutex_init(&pool->mutex, NULL);
 
+	//插入numWorkers个任务到任务队列pool->workers中
 	int i=0;
-	for(i=0;i<numWorkers;++i){
+	for(i=0; i<numWorkers; ++i){
 		struct nWorker* worker = (struct nWorker*)malloc(sizeof(struct nWorker));
 		if(worker == NULL){
 			perror("malloc");
@@ -87,6 +92,7 @@ int nThreadPoolCreate(ThreadPool* pool,int numWorkers){
 		memset(worker,0,sizeof(struct nWorker));
 
 		worker->manager = pool;
+		worker->terminate = 0;
 
 		int ret=pthread_create(&worker->threadid,NULL,nThreadPoolCallback,worker);
 		if(ret){
@@ -108,8 +114,10 @@ int nThreadPoolDestory(ThreadPool* pool,int nWorker){
 	for(worker = pool->workers;worker != NULL;worker = worker->next){
 		worker->terminate = 1;
 	}
-	pthread_mutex_lock(&pool->mutex);
 
+	//与回调函数里的锁要是同一把锁，否则会出现死锁现象
+	pthread_mutex_lock(&pool->mutex);
+	//API的作用是唤醒所有阻塞在条件变量pool->cond上的全部线程
 	pthread_cond_broadcast(&pool->cond);
 
 	pthread_mutex_unlock(&pool->mutex);
@@ -123,7 +131,7 @@ int nThreadPoolPushTask(ThreadPool* pool,struct nTask* task){
 	pthread_mutex_lock(&pool->mutex);	
 
 	LIST_INSERT(task,pool->tasks);
-
+	//API的作用是唤醒一个阻塞在条件变量pool->cond上的线程
 	pthread_cond_signal(&pool->cond);
 
 	pthread_mutex_unlock(&pool->mutex);
